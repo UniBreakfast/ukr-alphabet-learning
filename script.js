51be87c2fe1f14ae8689ab33c08c6c85fbc7e1bf
pikitosdoritos <fastovetsn@gmail.com>
2026-07-28 18:20:12 +0300

    refactor: update getWords function to initialize words array and log results

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const words = []
+const words = await getWords()
 
+console.log(words)
 
-const response = await fetch('words.txt')
-const text = await response.text()
-const lines = text.split(/\r?\n/).filter(Boolean).map(line => line.trim())
+async function getWords() {
+    const response = await fetch('words.txt')
+    const text = await response.text()
+    const lines = text.split(/\r?\n/).filter(Boolean).map(line => line.trim())
+    const words = []
 
-console.log(lines)
+    let category = ''
+
+    for (const line of lines) {
+        if (line == line.toUpperCase()) {
+            category = line.toLowerCase()
+        } else {
+            const word = line
+            words.push({ word, category })
+        }
+    }
+
+    return words
+}
