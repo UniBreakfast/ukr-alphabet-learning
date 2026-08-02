106003d9c41e52a6d03bb57faa2c19791a13fee3
UniBreakfast <unibreakfast1@gmail.com>
2026-08-02 13:14:02 +0300

    fix: ensure unique items are added to usedItems in getRandomItems function

--- script.js
+++ script.js
@@ -163,7 +163,10 @@ function getRandomItems(arg, count, usedItems = []) {
         const i = Math.floor(Math.random() * arr.length)
         const item = arr[i]
 
-        if (!usedItems.includes(item)) items.push(item)
+        if (!usedItems.includes(item)) {
+            items.push(item)
+            usedItems.push(item)
+        }
 
         arr.splice(i, 1)
     }
